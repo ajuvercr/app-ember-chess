d5b4a8624aa8682e0bc16f5a6e82974095aa1ce5
ajuvercr <arthur.vercruysse@outlook.com>
2021-06-30 09:16:52 +0200

    finish chapter 9

--- app/app.js
+++ app/app.js
@@ -10,3 +10,35 @@ export default class App extends Application {
 }
 
 loadInitializers(App, config.modulePrefix);
+
+
+const alreadyShownFactory = () => {
+  let alreadyShown = [];
+  return (msg, test, opt) => {
+    if (test)
+      return false;
+
+    if( alreadyShown.indexOf(msg) === -1 ) {
+      let warning = 'DEPRECATION: ' + msg;
+      if(opt && opt.url) {
+        warning += ' See: ' + opt.url;
+      }
+      console.warn(warning);
+      alreadyShown.push(msg);
+    }
+  };
+};
+
+Ember.deprecate = alreadyShownFactory();
+Ember.warn = alreadyShownFactory();
+
+//see https://guides.emberjs.com/v2.3.0/configuring-ember/handling-deprecations/
+Ember.Debug.registerDeprecationHandler((() => {
+  let alreadyShown = [];
+  return (message, options, next) => {
+    if(alreadyShown.indexOf(message) === -1) {
+      next(message, options);
+      alreadyShown.push(message);
+    }
+  };
+})());
